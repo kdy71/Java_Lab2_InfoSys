1c32a21bb760e1c2c5ab9b4cd70372c96f0480e7
Dmitry Khoruzhenko <kdy71@yandex.ru>
2016-03-30 21:31:10 +0300

    Появился clientConfig.xml - в нём можно задать параметры подключения к серверу (хост, порт).

--- Lab2_client/src/main/java/client/model/ISClient.java
+++ Lab2_client/src/main/java/client/model/ISClient.java
@@ -3,24 +3,33 @@ package client.model;
 import common_model.Util_msg;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.nio.charset.Charset;
 
 /**
- * Created by Dmitry Khoruzhenko on 19.02.2016.
+ * Created by Dmitry Khoruzhenko, Oleksandr Dudkin on 19.02.2016.
  * Information System Client
  * Здесь создаётся клиентский сокет.
  * И прописаны методы для отправки сообщений на сервер.
  */
 public class ISClient {
     private String serverHostName = "localhost";
-    private int serverPort = 50001;
+    private Integer serverPort = 50001;
     private InputStream is = null;
     private OutputStreamWriter osw = null;
     Socket socket = null;
 
+//    private static String SERVER_PARAMS = ".\\Lab2_client\\src\\main\\java\\client\\model\\clientConfig.xml";
+    private static String SERVER_PARAMS = "clientConfig.xml";
     public static final Logger log = LogManager.getLogger(ISClient.class);
 
 
@@ -29,11 +38,34 @@ public class ISClient {
      */
     public ISClient() {
         super();
+        setServerNameAndPortFromXml();
         startClient();
         log.info("ISClient constructor");
     }
 
 
+    /**
+     * Достает из файла и устанавливает параметры сервера, к которому нужно подключиться клиенту.
+     */
+    public void setServerNameAndPortFromXml() {
+        try {
+            Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(SERVER_PARAMS);
+            NodeList nodes = document.getDocumentElement().getChildNodes();
+            for (int i = 0; i < nodes.getLength(); i++) {
+                if ("serverName".equals(nodes.item(i).getNodeName())) {
+                    serverHostName = nodes.item(i).getTextContent();
+                }
+                if ("serverPort".equals(nodes.item(i).getNodeName())) {
+                    serverPort = Integer.valueOf(nodes.item(i).getTextContent());
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            Util_msg.showError("Ошибка при обработке файла с параметрами сервера.\n" + e.getMessage()+
+            "\n\n Будут применены параметры сервера по умолчанию: "+serverHostName+":"+serverPort);
+//            e.printStackTrace();
+            log.error("Exception while parsing xml-file with server name and port." + e);
+        }
+    }
     /**
      * Возвращает InputStream для клиентского сокета
      * @return  InputStream для клиентского сокета
